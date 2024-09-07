d8ae6ce805f6f0365d5cb03fe0cd7a9604ec3baf
Cyrill Raccaud <cyrill.raccaud@aposphere.com>
2024-09-07 13:57:48 +0200

    > 1.0.0-rc.2

--- linux2mqtt/__init__.py
+++ linux2mqtt/__init__.py
@@ -1,6 +1,6 @@
 """linux2mqtt package."""
 
-__VERSION__ = "1.0.0-rc.1"
+__VERSION__ = "1.0.0-rc.2"
 
 from .const import (
     DEFAULT_CONFIG,
---
+++ linux2mqtt/__main__.py
@@ -0,0 +1,6 @@
+"""linux2mqtt main entry."""
+
+from .linux2mqtt import main
+
+if __name__ == "__main__":
+    main()
